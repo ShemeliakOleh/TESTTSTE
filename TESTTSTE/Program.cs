e1b0b5e958c20cc3699633f4b3f7033c521b34a7
DESKTOP-LFA7BBV\MaDPC <shemeliak.oleh.main@gmail.com>
2021-11-15 16:46:34 +0200

    Add project files.

---
+++ TESTTSTE/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using MathWorks.MATLAB.NET.Arrays;
+namespace TESTTSTE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LibForChMMF.LibForChMMF libclass = new LibForChMMF.LibForChMMF();
+            try
+            {
+                var s = libclass.Draw("C:\\Source\\Triangle.NET\\FEMSolver\\bin\\Debug\\net5.0\\Test1.txt");
+
+            }
+            catch (Exception)
+            {
+
+               
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
